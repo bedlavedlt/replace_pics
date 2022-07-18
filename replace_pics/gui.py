c85444c02d2c40051a745ae590e28a65b1237574
bedlavedlt <bedlavedlt@gmail.com>
2022-07-17 19:32:44 -0500

    working on file generator

--- replace_pics/gui.py
+++ replace_pics/gui.py
@@ -1,5 +1,5 @@
 from os.path import exists
-from tkinter import Tk, PhotoImage, TOP, BOTTOM, END
+from tkinter import Tk, PhotoImage, TOP, BOTTOM, END, LEFT
 from tkinter.ttk import Frame, Button, Entry
 from tkinter.filedialog import askdirectory
 
@@ -29,9 +29,22 @@ class Application(Tk):
         # Directory Text 
         self.csv_entry = Entry(self.top_top_frame, text='')
         self.csv_entry.pack(fill='x', expand=True)
-        # CSV Button
-        self.select_csv_button = Button(self.top_top_frame, text='Select Directory', command=select_directory)
-        self.select_csv_button.pack(anchor='e')
+
+        # Select Directory Button
+        self.select_directory_button = Button(self.top_top_frame, text='Select Directory', command=select_directory)
+        self.select_directory_button.pack(anchor='e')
+
+        # Buttons
+        self.next_button = Button(self.top_bottom_frame, text="Next", command=next_image)
+        self.next_button.pack(side=LEFT)
+
+        self.previous_button = Button(self.top_bottom_frame, text="Previous", command=previous_image)
+        self.previous_button.pack(side=LEFT)
+
+        self.replace_button = Button(self.top_bottom_frame, text="replace", command=replace_image)
+        self.replace_button.pack(side=LEFT)
+
+        
 
 
         def select_directory(self):
@@ -39,3 +52,16 @@ class Application(Tk):
             if exists(self.output_path):
                 self.output_entry.delete(0, END)
                 self.output_entry.insert(0, self.output_path)
+                
+        def next_image(self):
+            # Image
+            image = 0
+            for i in range(image):
+                
+            self.image = PhotoImage(image)
+
+        def previous_image(self):
+            pass
+    
+        def replace_image(self):
+            pass
